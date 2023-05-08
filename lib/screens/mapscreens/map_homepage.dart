8e7eaa285c4452b1f1e414f269e1115f6a6672e0
sohebalam <sohebalam@hotmail.com>
2023-05-08 12:18:19 +0100

    drawer

--- lib/screens/mapscreens/map_homepage.dart
+++ lib/screens/mapscreens/map_homepage.dart
@@ -19,10 +19,21 @@ class _MapHomePageState extends State<MapHomePage> {
         title: Text('Map Home Page'),
       ),
       drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
         child: ListView(
-          children: <Widget>[
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
             ListTile(
-              title: Text('Register Parking Space'),
+              title: const Text('Register Parking Space'),
               onTap: () {
                 Navigator.push(
                   context,
@@ -30,6 +41,16 @@ class _MapHomePageState extends State<MapHomePage> {
                     builder: (context) => RegisterParkingSpace(),
                   ),
                 );
+                // Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
               },
             ),
           ],
