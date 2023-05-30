5daf39df0aed082945c1c520b7f231a36f589bd0
sohebalam <sohebalam@hotmail.com>
2023-05-31 00:16:34 +0100

    fixes

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -43,339 +43,6 @@ class AuthScreen extends StatefulWidget {
   _AuthScreenState createState() => _AuthScreenState();
 }
 
-// class _AuthScreenState extends State<AuthScreen> {
-//   GoogleSignIn googleSignIn = GoogleSignIn();
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-//   FirebaseFirestore firestore = FirebaseFirestore.instance;
-//   final TextEditingController emailController = TextEditingController();
-//   final TextEditingController passwordController = TextEditingController();
-//   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-//   bool _obscureText = true;
-//   String _errorMessage = '';
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     print('widget.routePage');
-//     print(widget.routePage);
-//   }
-
-//   Future<void> signInFunction(BuildContext context, String? routePage,
-//       {required DateTime startDateTime,
-//       required DateTime endDateTime,
-//       required String cpsId,
-//       String? address,
-//       String? postcode,
-//       String? image}) async {
-//     GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-//     if (googleUser == null) {
-//       return;
-//     }
-//     final googleAuth = await googleUser.authentication;
-//     final credential = GoogleAuthProvider.credential(
-//         accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
-//     UserCredential userCredential =
-//         await FirebaseAuth.instance.signInWithCredential(credential);
-
-//     DocumentSnapshot userExist =
-//         await firestore.collection('users').doc(userCredential.user!.uid).get();
-//     DocumentSnapshot userEmailExist = await firestore
-//         .collection('users')
-//         .doc(userCredential.user!.email)
-//         .get();
-
-//     if (userExist.exists || userEmailExist.exists) {
-//       print("User Already Exists in Database");
-//     } else {
-//       await firestore.collection('users').doc(userCredential.user!.uid).set({
-//         'email': userCredential.user!.email,
-//         'name': userCredential.user!.displayName,
-//         'image': userCredential.user!.photoURL,
-//         'uid': userCredential.user!.uid,
-//         'date': DateTime.now(),
-//       });
-//     }
-
-//     Navigator.pushReplacement(
-//       context,
-//       MaterialPageRoute(
-//         builder: (context) => Booking(
-//           startDateTime: startDateTime,
-//           endDateTime: endDateTime,
-//           cpsId: cpsId,
-//           address: address!,
-//           image: image!,
-//           postcode: postcode!,
-//         ),
-//       ),
-//     );
-//   }
-
-//   Future<void> signInFunc(
-//     BuildContext context,
-//   ) async {
-//     GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-//     if (googleUser == null) {
-//       return;
-//     }
-//     final googleAuth = await googleUser.authentication;
-//     final credential = GoogleAuthProvider.credential(
-//         accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
-//     UserCredential userCredential =
-//         await FirebaseAuth.instance.signInWithCredential(credential);
-
-//     DocumentSnapshot userExist =
-//         await firestore.collection('users').doc(userCredential.user!.uid).get();
-//     DocumentSnapshot userEmailExist = await firestore
-//         .collection('users')
-//         .doc(userCredential.user!.email)
-//         .get();
-
-//     if (userExist.exists || userEmailExist.exists) {
-//       print("User Already Exists in Database");
-//     } else {
-//       await firestore.collection('users').doc(userCredential.user!.uid).set({
-//         'email': userCredential.user!.email,
-//         'name': userCredential.user!.displayName,
-//         'image': userCredential.user!.photoURL,
-//         'uid': userCredential.user!.uid,
-//         'date': DateTime.now(),
-//       });
-//     }
-//     Navigator.pushReplacement(
-//       context,
-//       MaterialPageRoute(
-//         builder: (context) => MapHome(),
-//       ),
-//     );
-//   }
-
-//   // }
-
-//   void _login({required String routePage}) async {
-//     // if (_formKey.currentState!.validate()) {
-//     // _formKey.currentState!.save();
-
-//     try {
-//       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-//         email: emailController.text,
-//         password: passwordController.text,
-//       );
-//       if (routePage == 'results_page') {
-//         Navigator.push(
-//           context,
-//           MaterialPageRoute(
-//             builder: (context) => Booking(
-//               startDateTime: widget.startdatetime,
-//               endDateTime: widget.enddatetime,
-//               cpsId: widget.cpsId,
-//               address: '',
-//               image: '',
-//               postcode: '',
-//             ),
-//           ),
-//         );
-//       } else if (widget.routePage == 'chat_page') {
-//         print('dchjadjas');
-//         if (widget.u_id != null) {
-//           Navigator.push(
-//             context,
-//             MaterialPageRoute(
-//               builder: (context) => ChatPage(
-//                 u_id: widget.u_id!,
-//                 currentUserId: userCredential.user!.uid,
-//               ),
-//             ),
-//           );
-//         }
-//       }
-//       print('User logged in: ${userCredential.user!.email}');
-//     } catch (error) {
-//       setState(() {
-//         _errorMessage = error.toString();
-//       });
-//       print('Error: $_errorMessage');
-//     }
-//     // }
-//   }
-
-//   Widget build(BuildContext context) {
-//     final authService = Provider.of<AuthService>(context);
-//     final isLoggedInStream = authService.user!.map((user) => user != null);
-//     print('results:');
-//     // print(widget.results);
-//     return Scaffold(
-//       appBar: CustomAppBar(title: 'Login', isLoggedInStream: isLoggedInStream),
-//       // drawer: AppDrawer(),
-//       body: SingleChildScrollView(
-//         child: Center(
-//           child: Column(
-//             children: [
-//               SizedBox(height: 80),
-//               Column(
-//                 crossAxisAlignment: CrossAxisAlignment.center,
-//                 children: [
-//                   Container(
-//                     width: 180, // Adjust the width as needed
-//                     height: 180, // Adjust the height as needed
-//                     decoration: BoxDecoration(
-//                       image: DecorationImage(
-//                         image: AssetImage('assets/parking.png'),
-//                         fit: BoxFit.cover,
-//                       ),
-//                     ),
-//                   ),
-//                   SizedBox(height: 16), // Add space between the image and text
-//                   Padding(
-//                     padding: EdgeInsets.only(
-//                         right: 15), // Adjust the left padding as needed
-//                     child: Text(
-//                       'Spare Park',
-//                       style: TextStyle(
-//                         fontSize: 18,
-//                         fontWeight: FontWeight.bold,
-//                         color: Colors.black,
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//               SizedBox(height: 20),
-//               Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   Padding(
-//                     padding: const EdgeInsets.all(8.0),
-//                     child: TextField(
-//                       controller: emailController,
-//                       decoration: InputDecoration(
-//                         labelText: "Email",
-//                       ),
-//                     ),
-//                   ),
-//                   Padding(
-//                     padding: const EdgeInsets.all(8.0),
-//                     child: TextField(
-//                       controller: passwordController,
-//                       decoration: InputDecoration(
-//                           labelText: "Password",
-//                           suffixIcon: IconButton(
-//                             icon: Icon(
-//                               _obscureText
-//                                   ? Icons.visibility_off
-//                                   : Icons.visibility,
-//                               color: _obscureText
-//                                   ? Colors.grey
-//                                   : Constants().primaryColor,
-//                             ),
-//                             onPressed: () {
-//                               setState(() {
-//                                 _obscureText = !_obscureText;
-//                               });
-//                             },
-//                           )),
-//                       obscureText: _obscureText,
-//                     ),
-//                   ),
-//                   SizedBox(height: 20),
-//                   ElevatedButton(
-//                     style: ElevatedButton.styleFrom(
-//                       backgroundColor: Constants()
-//                           .primaryColor, // Set a different color for the button
-//                     ),
-//                     onPressed: () {
-//                       if (emailController.text.length > 6 &&
-//                           passwordController.text.length > 6) {
-//                         _login(routePage: 'results_page');
-//                         // authService.signInWithEmailAndPassword(
-//                         //   context,
-//                         //   emailController.text,
-//                         //   passwordController.text,
-//                         //   // prior_page: widget.prior_page,
-//                         //   // location: widget.location,
-//                         //   // results: widget.results,
-//                         //   startdatetime: widget.startdatetime,
-//                         //   enddatetime: widget.enddatetime,
-//                         // );
-//                       } else {
-//                         ScaffoldMessenger.of(context).showSnackBar(
-//                           SnackBar(
-//                             content: Text(
-//                               "Email and password must be at least 6 characters long.",
-//                             ),
-//                           ),
-//                         );
-//                       }
-//                     },
-//                     child: Text(
-//                       'Login',
-//                     ),
-//                   ),
-//                   TextButton(
-//                     onPressed: () {
-//                       Navigator.pushReplacement(
-//                         context,
-//                         MaterialPageRoute(
-//                           builder: (context) => RegisterScreen(
-//                             // prior_page: widget.prior_page,
-//                             // location: widget.location,
-//                             // results: widget.results,
-//                             startdatetime: widget.startdatetime,
-//                             enddatetime: widget.enddatetime,
-//                           ),
-//                         ),
-//                       );
-//                     },
-//                     child: Text('Register',
-//                         style: TextStyle(
-//                           color: Constants().primaryColor,
-//                         )),
-//                   ),
-//                 ],
-//               ),
-//               SizedBox(height: 70),
-//               Padding(
-//                 padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
-//                 child: Center(
-//                   child: GoogleAuthButton(
-//                     onPressed: () async {
-//                       if (widget.routePage == 'results_page') {
-//                         await signInFunction(
-//                           context,
-//                           widget.routePage,
-//                           startDateTime: widget.startdatetime,
-//                           endDateTime: widget.enddatetime,
-//                           cpsId: widget.cpsId,
-//                           address: widget.address,
-//                           postcode: widget.postcode,
-//                           image: widget.image,
-//                         );
-//                       } else {
-//                         await signInFunc(context);
-//                       }
-//                     },
-//                     text: "Sign up with Google",
-//                     style: AuthButtonStyle(
-//                       width: 350,
-//                       height: 60,
-//                       iconType: AuthIconType.outlined,
-//                       buttonColor: Colors.white,
-//                       textStyle: TextStyle(
-//                         fontSize: 12,
-//                         color: Colors.black,
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 class _AuthScreenState extends State<AuthScreen> {
   GoogleSignIn googleSignIn = GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -629,53 +296,98 @@ class _AuthScreenState extends State<AuthScreen> {
                     ),
                   ),
                   SizedBox(height: 20),
+
+                  //                   SizedBox(height: 20),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      primary: Constants().primaryColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                      ),
+                      backgroundColor: Constants()
+                          .primaryColor, // Set a different color for the button
                     ),
                     onPressed: () {
-                      _login(routePage: widget.routePage!);
+                      if (emailController.text.length > 6 &&
+                          passwordController.text.length > 6) {
+                        _login(routePage: widget.routePage!);
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              "Email and password must be at least 6 characters long.",
+                            ),
+                          ),
+                        );
+                      }
                     },
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 10,
-                      ),
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
+                    child: Text(
+                      'Login',
                     ),
                   ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Don't have an account? "),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => RegisterScreen(),
-                            ),
-                          );
-                        },
-                        child: Text(
-                          "Sign Up",
-                          style: TextStyle(
-                            color: Constants().primaryColor,
-                            fontWeight: FontWeight.bold,
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => RegisterScreen(
+                            // prior_page: widget.prior_page,
+                            // location: widget.location,
+                            // results: widget.results,
+                            startdatetime: widget.startdatetime,
+                            enddatetime: widget.enddatetime,
                           ),
                         ),
-                      ),
-                    ],
+                      );
+                    },
+                    child: Text('Register',
+                        style: TextStyle(
+                          color: Constants().primaryColor,
+                        )),
                   ),
+                  // ElevatedButton(
+                  //   style: ElevatedButton.styleFrom(
+                  //     primary: Constants().primaryColor,
+                  //     shape: RoundedRectangleBorder(
+                  //       borderRadius: BorderRadius.circular(18.0),
+                  //     ),
+                  //   ),
+                  //   onPressed: () {
+                  //     _login(routePage: widget.routePage!);
+                  //   },
+                  //   child: Padding(
+                  //     padding: EdgeInsets.symmetric(
+                  //       horizontal: 20,
+                  //       vertical: 10,
+                  //     ),
+                  //     child: Text(
+                  //       "Login",
+                  //       style: TextStyle(
+                  //         fontSize: 20,
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
+                  // SizedBox(height: 10),
+                  // Row(
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   children: [
+                  //     Text("Don't have an account? "),
+                  //     GestureDetector(
+                  //       onTap: () {
+                  //         Navigator.push(
+                  //           context,
+                  //           MaterialPageRoute(
+                  //             builder: (context) => RegisterScreen(),
+                  //           ),
+                  //         );
+                  //       },
+                  //       child: Text(
+                  //         "Sign Up",
+                  //         style: TextStyle(
+                  //           color: Constants().primaryColor,
+                  //           fontWeight: FontWeight.bold,
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
                   SizedBox(height: 10),
                   _errorMessage.isNotEmpty
                       ? Text(
@@ -687,7 +399,7 @@ class _AuthScreenState extends State<AuthScreen> {
                       : Container(),
                 ],
               ),
-              SizedBox(height: 10),
+              SizedBox(height: 70),
               Padding(
                   padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
                   child: Center(
